72fd9a1662dd816a548889286d270b328c35a942
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-10-03 03:53:18 +0530

    Add Karatsuba's algorithm

--- cat2/Karatsuba.java
+++ cat2/Karatsuba.java
@@ -4,23 +4,43 @@ import java.util.*;
 // import java.io.*;
 
 class Karatsuba {
-    public static int kMult(int a, int b) {
+
+    // for eg 
+    // 146123 == 146 * 10 ^ 3 + 123
+    // x = a * 10^n/2 + b
+    // y = c * 10^n/2 + d
+    // by distributing the multiplication:
+    // so
+    // xy = ac * 10^2(n/2) + (ad + bc) * (10^n/2) + bd
+    // ac and bd are recursive calls. ad + bc = k(a+b, c+d)-ac-bd
+    public static long kMult(long a, long b) {
         if (a < 10 || b < 10) return a * b;
 
-        int result = 1;
+        int length = Math.max(Long.toString(a).length(), Long.toString(b).length());
+        int halflen = length / 2;
 
+        long A = a / (long) Math.pow(10, halflen);
+        long B = a % (long) Math.pow(10, halflen);
+        long C = b / (long) Math.pow(10, halflen);
+        long D = b % (long) Math.pow(10, halflen);
 
+        long ac = kMult(A, C);
+        long bd = kMult(B, D);
 
-        return result;
+        long ad_bd = kMult(A+B, C+D) - ac - bd;
+
+        return ac * (long)Math.pow(10, 2 * halflen) + (ad_bd * (long)Math.pow(10, halflen)) + bd;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        long a = sc.nextInt();
+        long b = sc.nextInt();
 
-        int c = kMult(a, b);
+        long c = kMult(a, b);
+        long cnorm = a * b;
         System.out.println(c);
+        System.out.println(cnorm);
 
         sc.close();
     }
