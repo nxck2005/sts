deeddee12952430c631bdae21cc000b3e4023c54
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-10-03 05:14:50 +0530

    Add K flips

---
+++ cat2/LongestOne.java
@@ -0,0 +1,29 @@
+package cat2;
+
+import java.util.*;
+
+public class LongestOne {
+    public static void main(String[] args) {
+        int[] arr = {1,1,0,0,1,1,1,1,1};
+
+        int maxconsone = 0;
+        int start = 0;
+        int k = 1; // only 1 flip
+        int zerocount = 0;
+
+        for (int end = 0; end < arr.length; end++) {
+            if (arr[end] == 0) {
+                zerocount++;
+            }
+
+            while (zerocount > k) {
+                if (arr[start] == 0) {
+                    zerocount--;
+                }
+                start++;
+            }
+            maxconsone = Math.max(maxconsone, end-start+1);
+        }
+        System.out.println(maxconsone);
+    }
+}
