e4592fe7a3d644b05fa778a5586ce3f7164a6255
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-10-03 05:19:41 +0530

    Add Euclids algorithm

---
+++ cat2/Euclid.java
@@ -0,0 +1,14 @@
+package cat2;
+
+public class Euclid {
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 17;
+
+        System.out.println(gcd(a,b));
+    }
+}
